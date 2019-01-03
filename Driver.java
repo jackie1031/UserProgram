edd1d5a8314452b085f5766e1bd43f31399c902c
Suyi Liu <sliu92@jhu.edu>
2019-01-02 23:16:02 -0800

    added implementation of fully specified fsm function

--- Driver.java
+++ Driver.java
@@ -84,11 +84,129 @@ public class Driver {
             arrayOfVariableNames[i] = keyboard.nextLine();
         }
 
+        // ask user how many inputs are there
         System.out.println("========Now asking for inputs ========");
         System.out.println("How many inputs are there?");
+        int numInputs = keyboard.nextInt();
+        keyboard.nextLine();
+        String[] arrayOfInputs = new String[numInputs];
 
-        // ask for inputs names and possible input values ??
+        // ask for inputs names and possible input values
+        for (int i = 0; i < numInputs; i++) {
+            System.out.println("Please enter the value of input #" + (i+1) + ":");
+            arrayOfInputs[i] = keyboard.nextLine();
+        }
 
+        // ask for information on states and transitions
+        System.out.println("========Now asking for information on states and transitions========");
+        System.out.println("(Please notice that state #1 is the initial state)");
+        
+        /* Get the info for the first state */
+        // Get the state name for the first state
+        System.out.println("Please enter the name of state #1:");
+        String stateName = keyboard.nextLine();
+        State newState = new State(numVariablesInEachState, stateName);
+
+        // Get the initial values of variables in the first state
+        for (int i = 0; i < numVariablesInEachState; i++) {
+            System.out.println("Please enter initial value for variable " + arrayOfVariableNames[i] + " in state " + stateName + ":");
+            int initialValue = keyboard.nextInt();
+            Variable newVariable = new Variable(arrayOfVariableNames[i], initialValue);
+            newState.addVariableToArray(newVariable);
+        }
+
+        // Add the first state to the queue and set
+        q.add(newState);
+        userInputParser.addStateToMap(newState.encodeToString(), newState);
+        userInputParser.addState(newState);
+
+        int stateIndex = 2;
+        int answer = 1;
+        keyboard.nextLine();
+
+        // Main loop
+        while (!q.isEmpty()) {
+            // Pop out the first state in the waiting queue
+            State currState = q.poll();
+            // Ask for transitions regarding the current state
+            for (int transitionIndex = 0; transitionIndex < numInputs; transitionIndex++) {
+
+                // Get input
+                System.out.println("The input name for transition #" + (transitionIndex+1) + " in state " + currState.getStateName() + "is: " + arrayOfInputs[transitionIndex]);
+                String inputName = arrayOfInputs[transitionIndex];
+
+                // Skip this transition if such transition does not exist
+                System.out.println("Does such transition corresponding to input " + arrayOfInputs[transitionIndex] + " exist for state " + currState.getStateName() + "? enter 1 for yes, enter 0 for no");
+                answer = keyboard.nextInt();
+                keyboard.nextLine();
+                if (answer == 0) {
+                    continue;
+                }
+
+                // No need to check duplicate inputs here
+                
+                // Get output
+                System.out.println("Please enter output name for transition #" + (transitionIndex+1) + " in state " + currState.getStateName() 
+                    + ", corresponding to input " + arrayOfInputs[transitionIndex] + ":");
+                String outputName = keyboard.nextLine();
+
+                // Get encoded destination
+                System.out.println("Now asking for internal memory of destination state");
+                Variable[] newArrayOfVariables = new Variable[numVariablesInEachState];
+                for (int i = 0; i < numVariablesInEachState; i++) {
+                    System.out.println("Please enter initial value for variable " + arrayOfVariableNames[i] + " in the destination state:");
+                    int initialValue = keyboard.nextInt();
+                    Variable newVariable = new Variable(arrayOfVariableNames[i], initialValue);
+                    newArrayOfVariables[i] = newVariable;
+                }
+
+                String encodedDest = State.encodeArrayToString(newArrayOfVariables, numVariablesInEachState);
+                keyboard.nextLine();
+
+                // Get destination
+                State destState = userInputParser.getStateByName(encodedDest);
+
+
+                // if input & destination state the same?
+
+
+                // If we have not seen such destination state in the set
+                if (destState == null) {
+                    // Create such new state
+                    System.out.println("Destination state does not exist, creating a new state object");
+                    // Get the state name
+                    System.out.println("Please enter destination state name for such transition #" + transitionIndex + ": <" + inputName + "/" + outputName + "> in state encoded as: " + encodedDest);
+                    String destStateName = keyboard.nextLine();
+                    
+                    // check duplicate state name!!
+                    Set<String> allStateNames = userInputParser.getListOfAllStateName();
+                    while(allStateNames.contains(destStateName)) {
+                        System.out.println("!!!Warning!!! Duplicate state name, please enter again for such transition #" + transitionIndex + ": <" + inputName + "/" + outputName + "> in state encoded as: " + encodedDest);
+                        destStateName = keyboard.nextLine();
+
+                    }
+
+                    State newDestState = new State(numVariablesInEachState, destStateName);
+
+                    // Assign the array to the destination state
+                    newDestState.setArrayOfVariables(newArrayOfVariables);
+
+                    // Add it to the queue as well as the set
+                    q.add(newDestState);
+                    userInputParser.addStateToMap(encodedDest, newDestState);
+                    userInputParser.addState(newDestState);
+
+                    // Assign destState again
+                    destState = newDestState;
+                }
+
+                // Adding transition
+                userInputParser.addInput(inputName);
+                userInputParser.addOutput(outputName);
+                currState.addTransition(inputName, outputName, destState);
+                System.out.println("Transition <" + inputName + "/" + outputName + "---> " + destState.encodeToString() +"> added to state" + currState.getStateName());
+            }
+        }  
     }
 
     private static void notFullyFSM (){
@@ -220,6 +338,7 @@ public class Driver {
             } while (answer != 0);
         }    
     }
+
     public static void printInfo(String outputFile) throws IOException {
     	System.out.println("========Printing all the states========");
     	userInputParser.printListOfStates(outputFile);
